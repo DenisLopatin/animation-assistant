f7f7787c993abd4033ade28a68859a2adb46d2c1
DenisLopatin <www.denis-lopatin@yandex.ru>
2021-03-01 03:55:22 +0300

    ref b first commit

--- src/scripts/AnimationAssistant.js
+++ src/scripts/AnimationAssistant.js
@@ -2,6 +2,7 @@ export default class AnimationAssistant {
     constructor(selector) {
         this.selector = selector;
         this.elements = AnimationAssistant.getElementsBySelector(this.selector);
+        this.offset = null;
     }
 
     static getElementsBySelector(selector) {
@@ -16,33 +17,22 @@ export default class AnimationAssistant {
         return Math.trunc((top / document.documentElement.clientHeight) * 100);
     }
 
-    addEventForGetOffset(element, offset) {
+    addEventForGetOffset(element) {
         const offsetTop = AnimationAssistant.getOffsetTop(element);
         const percentOfOffset = AnimationAssistant.getPercentOfOffset(offsetTop);
-        if (offset > percentOfOffset) {
-            this.a = 1;
+        if (this.offset > percentOfOffset) {
+            element.animationAssistantIsReady = true;
         }
     }
 
-    setAnimation(name, offset = null, timeout = 2000) {
-        if (offset) {
-            return this.promiseOffset(name, offset, timeout);
-        }
-        return this.promise(name, timeout);
-    }
-
-    promise(name, timeout = 2000) {
-        return new Promise((resolve) => {
-            this.elements.forEach((element) => {
-                name.forEach((className) => element.classList.toggle(className));
-            });
-            setTimeout(() => {
-                resolve(this.setAnimation.bind(this));
-            }, timeout);
+    setTrigger(offset) {
+        this.offset = offset;
+        this.elements.forEach((element) => {
+            window.addEventListener('scroll', this.addEventForGetOffset.bind(this, element));
         });
     }
 
-    promiseOffset(name, offset, timeout = 2000) {
+    setAnimation(name, timeout = 2000) {
         return new Promise((resolve) => {
             this.elements.forEach((element) => {
                 name.forEach((className) => element.classList.toggle(className));
--- src/scripts/animation-assistant.js
+++ src/scripts/animation-assistant.js
@@ -4,7 +4,8 @@ import 'animate.css/animate.css';
 import AnimationAssistant from './AnimationAssistant';
 
 const animationAssistant = new AnimationAssistant('block-first');
+animationAssistant.setTrigger(20);
 animationAssistant
-    .setAnimation(['animate__animated', 'animate__swing'], 20)
+    .setAnimation(['animate__animated', 'animate__swing'])
     .then((res) => res(['animate__shakeX', 'animate__swing']))
     .then((res) => res(['animate__bounceOut', 'animate__shakeX']));
